a162b8e064a0f21ed073a960b54e7138b45be327
Victor-Estrella <victorhcarracci@gmail.com>
2024-11-28 18:26:41 -0300

    Adição de componentes Chat, Informacao, Bio e ImagemInformacao; refatoração de Conversa para utilizar novos componentes

--- components/Informacao.vue
+++ components/Informacao.vue
@@ -1,24 +1,8 @@
 <template>
     <section class="w-4/12 p-6 flex flex-col items-center text-white font-jakarta">
         <div class="flex flex-col items-center overflow-y-auto">
-            <div class="flex justify-center">
-                <img src="../public/img/eu-2.jfif" alt="Avatar" class="rounded-2xl w-8/12" />
-            </div>
-            <div class="bg-[#171B1F] mt-10 rounded-2xl p-6 w-8/12">
-                <div class="flex justify-between">
-                    <p class="text-2xl font-bold">André</p>
-                    <p class="text-2xl font-medium">18</p>
-                </div>
-                <p class="text-sm text-gray-400">São Paulo, SP</p>
-                <div class="mt-4 space-y-2 text-[#CFCFCF] font-medium text-lg">
-                    <p><strong class="text-[#e1e1e1]">Instituição:</strong> FIAP</p>
-                    <p><strong class="text-[#e1e1e1]">Curso:</strong> Análise e Desenvolvimento de Sistemas</p>
-                    <p><strong class="text-[#e1e1e1]">Certificações:</strong> Front-end FIAP, Banco de Dados FIAP, JavaScript Alura, React
-                        (formação completa Alura)</p>
-                    <p><strong class="text-[#e1e1e1]">Conhecimento:</strong> Front e Back, React, MongoDB, Vue, Nuxt, JavaScript, Python, Java,
-                        SQL, MySQL, Next, HTML, CSS, Tailwind</p>
-                </div>
-            </div>
+            <InformacoesImagemInformacao/>
+            <InformacoesBio/>
         </div>
     </section>
 </template>
