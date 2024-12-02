d337a138ef551f31a19c622b1bb58e02dc771e60
andrealtobelli <andrerogerio03@gmail.com>
2024-12-02 14:13:34 -0300

    Adicionando a rota do sobre na página inicial da aplicação.

--- components/Home.vue
+++ components/Home.vue
@@ -4,7 +4,7 @@
             <h1 class="text-white text-3xl font-bold">Linksy</h1>
             <nav>
                 <ul class="flex gap-6">
-                    <li><a href="#" class="text-[#9B9C9E] hover:text-white transition duration-200">Sobre</a></li>
+                    <li><a href="/sobre" class="text-[#9B9C9E] hover:text-white transition duration-200">Sobre</a></li>
                     <li><a href="#" class="text-[#9B9C9E] hover:text-white transition duration-200">Ajuda</a></li>
                     <li><a href="/login" class="text-[#B6F09C] hover:underline">Login</a></li>
                 </ul>
