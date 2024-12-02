d65618fea51bc4f2fba14ec6e28735b406781c0a
andrealtobelli <andrerogerio03@gmail.com>
2024-12-02 13:58:31 -0300

    Header da Home realizado.

---
+++ components/Home.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="min-h-screen bg-[#131619] flex flex-col items-center justify-center font-jakarta">
+        <header class="w-full px-6 py-4 flex justify-between items-center">
+            <h1 class="text-white text-3xl font-bold">Linksy</h1>
+            <nav>
+                <ul class="flex gap-6">
+                    <li><a href="#" class="text-[#9B9C9E] hover:text-white transition duration-200">Sobre</a></li>
+                    <li><a href="#" class="text-[#9B9C9E] hover:text-white transition duration-200">Ajuda</a></li>
+                    <li><a href="/login" class="text-[#B6F09C] hover:underline">Login</a></li>
+                </ul>
+            </nav>
+        </header>
+        
+    </div>
+</template>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:ital,wght@0,200..800;1,200..800&display=swap');
+
+.font-jakarta {
+    font-family: 'Plus Jakarta Sans', sans-serif;
+}
+</style>
