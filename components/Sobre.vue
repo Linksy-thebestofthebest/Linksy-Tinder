81fbafd3097bbdc87f0737b57036213bed34431d
andrealtobelli <andrerogerio03@gmail.com>
2024-12-02 14:14:46 -0300

    Header da página sobre realizado.

---
+++ components/Sobre.vue
@@ -0,0 +1,22 @@
+<template>
+    <section class="min-h-screen bg-[#131619] text-white font-jakarta px-8 py-16">
+      <div class="max-w-6xl mx-auto">
+        <header class="text-center mb-12">
+          <h1 class="text-4xl font-bold mb-4">Sobre a Linksy</h1>
+          <p class="text-lg text-[#9B9C9E]">
+            Conectando talentos, criando oportunidades.
+          </p>
+        </header>
+  
+        </div>
+    </section>
+  </template>
+  
+  <style>
+  @import url('https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:ital,wght@0,200..800;1,200..800&display=swap');
+  
+  .font-jakarta {
+    font-family: 'Plus Jakarta Sans', sans-serif;
+  }
+  </style>
+  
