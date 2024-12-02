8cbbe67d16390a8ef16e65223fb330f22e536ef3
andrealtobelli <andrerogerio03@gmail.com>
2024-12-02 13:59:34 -0300

    Conteúdo principal da home adicionado.

--- components/Home.vue
+++ components/Home.vue
@@ -10,6 +10,22 @@
                 </ul>
             </nav>
         </header>
+        <main class="text-center flex-1 flex flex-col items-center justify-center">
+            <h2 class="text-white text-4xl md:text-6xl font-extrabold mb-6">
+                Conecte-se com <span
+                    class="bg-clip-text bg-gradient-to-tr from-[#4D62E5] via-[#87DDEE] to-[#B6F09C] text-transparent">profissionais</span>
+            </h2>
+            <p class="text-[#9B9C9E] text-lg md:text-xl mb-12 max-w-lg">
+                Transforme conexões em oportunidades. A Linksy é a forma moderna e dinâmica de expandir sua rede
+                profissional.
+            </p>
+            <div class="flex gap-6">
+                <button
+                    class="bg-gradient-to-tr from-[#4D62E5] via-[#87DDEE] to-[#B6F09C] text-[#0C1132] px-8 py-4 rounded-lg font-semibold text-lg shadow-lg hover:scale-105 transition transform duration-200">
+                    <a href="/cadastro">Comece Agora</a>
+                </button>
+            </div>
+        </main>
         
     </div>
 </template>
