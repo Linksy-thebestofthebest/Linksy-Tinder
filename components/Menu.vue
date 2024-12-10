ec7c7af840761798d0905ef6872b33589bc49f70
Victor-Estrella <victorhcarracci@gmail.com>
2024-12-10 17:26:15 -0300

    Criação de menu em mobile na pagina buscar

---
+++ components/Menu.vue
@@ -0,0 +1,7 @@
+<template>
+    <section class="flex absolute bottom-0 justify-evenly w-full mb-8 md:hidden">
+        <a href="/buscar"><img src="/img/buscar-mobile.svg" alt="Buscar" class="h-8"></a>
+        <a href="/conversas"><img src="/img/conversa-mobile.svg" alt="Conversa" class="h-8"></a>
+        <a href="/perfil"><img src="/img/perfil-mobile.svg" alt="Perfil" class="h-8"></a>
+    </section>
+</template>
